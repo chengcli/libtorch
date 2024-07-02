59398bc42b9d764064a860188edcd26b7e4484a0
mac/cli <chengcli@umich.edu>
2024-07-02 01:22:38 -0400

    add gamma

--- aten/src/ATen/native/euler.cpp
+++ aten/src/ATen/native/euler.cpp
@@ -10,40 +10,51 @@
 
 namespace at::native {
   TORCH_API at::Tensor & euler_cons2prim_cpu_out(
-      const at::Tensor & cons, at::Tensor & prim) {
+      const at::Tensor & cons, double gammad, at::Tensor & prim) 
+  {
     TORCH_CHECK(cons.is_contiguous(), "cons must be contiguous");
+    TORCH_CHECK(prim.is_contiguous(), "prim must be contiguous");
 
     auto cons_sizes = cons.sizes();
-    auto cons_strides = cons.strides();
-    auto cons_data = cons.data_ptr<float>();
 
+    TORCH_CHECK(cons_sizes[0] >= 5, "cons must have at least 5 fields");
+
+    auto cons_data = cons.data_ptr<float>();
     auto prim_data = prim.data_ptr<float>();
 
-    int64_t n_cells = cons_sizes[0];
-    int64_t n_fields = cons_sizes[1];
+    size_t ncells = 1;
+    for (size_t i = 1; i < cons_sizes.size(); i++) {
+      ncells *= cons_sizes[i];
+    }
 
-    for (int64_t i = 0; i < n_cells; i++) {
-      float rho = cons_data[i * cons_strides[0] + 0];
-      float mom_x = cons_data[i * cons_strides[0] + 1];
-      float mom_y = cons_data[i * cons_strides[0] + 2];
-      float mom_z = cons_data[i * cons_strides[0] + 3];
-      float energy = cons_data[i * cons_strides[0] + 4];
+    auto cons_strides = cons.strides();
+    int64_t nstride = cons_strides[0];
 
-      prim_data[i * cons_strides[0] + 0] = rho;
-      prim_data[i * cons_strides[0] + 1] = mom_x / rho;
-      prim_data[i * cons_strides[0] + 2] = mom_y / rho;
-      prim_data[i * cons_strides[0] + 3] = mom_z / rho;
-      prim_data[i * cons_strides[0] + 4] = (energy - 0.5 * (mom_x * mom_x + mom_y * mom_y + mom_z * mom_z) / rho) / rho;
+    for (size_t i = 0; i < ncells; i++) {
+      float rho = cons_data[i * nstride + 0];
+      float momx = cons_data[i * nstride + 1];
+      float momy = cons_data[i * nstride + 2];
+      float momz = cons_data[i * nstride + 3];
+      float energy = cons_data[i * nstride + 4];
+
+      prim_data[i * nstride + 0] = rho;
+      prim_data[i * nstride + 1] = momx / rho;
+      prim_data[i * nstride + 2] = momy / rho;
+      prim_data[i * nstride + 3] = momz / rho;
+
+      float ke = 0.5 * (momx * momx + momy * momy + momz * momz) / rho;
+      prim_data[i * nstride + 4] = (energy - ke) / (gammad - 1.);
     }
 
     return prim;
   }
 
-  TORCH_API at::Tensor euler_cons2prim_cpu(const at::Tensor & cons) {
+  TORCH_API at::Tensor euler_cons2prim_cpu(const at::Tensor & cons, double gammad)
+  {
     TORCH_CHECK(cons.is_contiguous(), "cons must be contiguous");
 
     Tensor prim = at::empty(cons.sizes(), cons.options());
-    euler_cons2prim_cpu_out(cons, prim);
+    euler_cons2prim_cpu_out(cons, gammad, prim);
 
     return prim;
   }
